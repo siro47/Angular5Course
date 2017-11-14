c81842caec48d75d7e1f9511804013038adc8285
sramirez <sramirez@solidgeargroup.com>
2017-11-14 23:47:59 +0100

    Added navigation between components

--- src/app/admin-layout/users/users.component.ts
+++ src/app/admin-layout/users/users.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+    id: string;
+    name: string;
+    desc: string;
+    image: string;
+
+    constructor(id: string, name: string, desc:string, image:string) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+    }
+}
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,9 +21,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+    title = 'USERS';
 
-  ngOnInit() {
-  }
+    users = [
+        new User("1", "Bart Simpson", "Always up to no good", "./assets/images/bart-simpson.png"),
+        new User("2", "Lisa Simpson", "The best of his class", "./assets/images/lisa-simpson.png"),
+        new User("3", "Homer Simpson", "D'oh!", "./assets/images/homer-simpson.png"),
+        new User("4", "Marge Simpson", "Great wife", "./assets/images/marge-simpson.png"),
+        new User("5", "Milhouse", "Nobody likes Milhouse!", "./assets/images/milhouse.jpeg"),
+        new User("6", "Ppal. Skinner", "I love my mum", "./assets/images/skinner.png"),
+    ]
 
+    public addNewUser() {
+        this.users.push(new User("7", "Moe", "Prettiest bartender ever", "./assets/images/moe.png"));
+    }
+
+    private removeUser(data) {
+        var index = this.users.findIndex(user => {return user.id == data.id});
+        this.users.splice(index, 1);
+    }
+
+    ngOnInit() {
+
+    }
 }
